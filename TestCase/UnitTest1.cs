b08aeb3f4a323d99b6bc354a7e17c3df08166778
hilmisidek <khairulhilmi.sidek@petronas.com>
2025-02-26 15:32:39 +0800

    add comment

--- TestCase/UnitTest1.cs
+++ TestCase/UnitTest1.cs
@@ -23,10 +23,18 @@ namespace qaplayground.TestCase
         [Test]
         public void Test1()
         {
+            //open url
             driver.Url = "https://qaplayground.dev/apps/dynamic-table/";
+            
+            //create new object
             dyntable page_0 = new dyntable();
+            
+            //initialize page
             PageFactory.InitElements(driver, page_0);
+
+            //using method in dyntable class
             var name_real = page_0.get_name();
+
             Console.WriteLine("NAME :::::");
             Console.WriteLine(name_real);
 
@@ -36,19 +44,26 @@ namespace qaplayground.TestCase
         [Test]
         public void Test2()
         {
-
+            //initialize new object from car class inherit vehicle class
             Car proton_satria = new Car();
+            
+            //this portion come from vehicle class
             proton_satria.make = "Proton";
             proton_satria.model = "Satria";
             proton_satria.type = "Car";
             proton_satria.color = "Blue";
             proton_satria.transmission = "Auto";
             proton_satria.seater = 6;
+
+            //this portion come from car class
             proton_satria.entertainment_item = "Bluetooth Player";
             proton_satria.autopilot = true;
             proton_satria.seat_cover = "Leather";
 
+            //this method come from vehicle class
             proton_satria.vehicle_info();
+
+            //this method come from car class
             proton_satria.car_info();
 
 
@@ -56,11 +71,18 @@ namespace qaplayground.TestCase
 
         [Test]
         public void Test3_string() {
-
+            //using built in function to reverse as string
             String input = "Awang goneng";
+
+            //cast string to array
             Char[] arr = input.ToCharArray();
+            
+            //reverse
             Array.Reverse(arr);
+
+            //initialize a new string
             String in_rev_str = new string(arr);
+            
             Console.WriteLine(in_rev_str);
 
         }
